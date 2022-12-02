f22ae71b13c404284ba5d476e0be9177be224a3c
filipton <filipton12@gmail.com>
2022-12-02 20:27:26 +0100

    Day2 optimized (inspiration: https://github.com/teraa/AoC.2022)

--- AdventOfCode/Day2.cs
+++ AdventOfCode/Day2.cs
@@ -11,84 +11,54 @@ public class Day2
 
         foreach (string line in lines)
         {
-            string[] inps = line.Split(' ');
+            char[] inps = line.Split(' ').Select(x => x[0]).ToArray();
+            int p1Index = inps[0] - 'A';
+            int p2Index = inps[1] - 'X';
             
-            // PART ONE
-            switch (inps[1])
-            {
-                case "X":
-                    score += 1;
-                    break;
-                case "Y":
-                    score += 2;
-                    break;
-                case "Z":
-                    score += 3;
-                    break;
-            }
-
-            
-            // PART TWO
-            score2 += GetPartTwoScore(inps[0], inps[1]);
-            
-            // PART ONE
-            score += CheckRound(inps[0], inps[1]);
+            score += PartOne(p1Index, p2Index);
+            score2 += PartTwo(p1Index, p2Index);
         }
         
-        Console.WriteLine($"Elf score: {score}");
-        Console.WriteLine($"Elf score 2: {score2}");
+        Console.WriteLine($"Part one: {score}");
+        Console.WriteLine($"Part two: {score2}");
     }
 
     
-    // PART ONE
-    static int CheckRound(string p1, string p2)
+    // PART ONE (INSPIRED BY https://github.com/teraa/AoC.2022/blob/master/Day02.a/Program.cs)
+    static int PartOne(int p1, int p2)
     {
-        //draw
-        if ((p1 == "A" && p2 == "X") ||
-            (p1 == "B" && p2 == "Y") ||
-            (p1 == "C" && p2 == "Z")) return 3;
+        // win - sub (mod 0)
+        // 0 1 - -1
+        // 1 2 - -1
+        // 2 0 - 2
         
-        // loss
-        if ((p1 == "A" && p2 == "Z") ||
-            (p1 == "B" && p2 == "X") ||
-            (p1 == "C" && p2 == "Y")) return 0;
+        // draw - 0 (mod 1)
         
+        // lose - sub (mod 2)
+        // 0 2 - -2
+        // 1 0 - 1
+        // 2 1 - 1
+        
+        int sub = p1 - p2 + 4;
+        return (p2 + 1) + (2 - sub % 3) * 3;
+    }
+    
+    
+    // PART TWO (INSPIRED BY https://github.com/teraa/AoC.2022/blob/master/Day02.b/Program.cs)
+    static int PartTwo(int p1, int p2)
+    {
         // win
-        if ((p1 == "A" && p2 == "Y") ||
-            (p1 == "B" && p2 == "Z") ||
-            (p1 == "C" && p2 == "X")) return 6;
-
-        return 0;
-    }
-    
-    
-    // PART TWO
-    static int GetPartTwoScore(string p1, string p2)
-    {
-        switch (p2)
-        {
-            // lose
-            case "X":
-                if (p1 == "A") return 3 + 0;
-                if (p1 == "B") return 1 + 0;
-                if (p1 == "C") return 2 + 0;
-                break;
-            
-            // draw
-            case "Y":
-                if (p1 == "A") return 1 + 3;
-                if (p1 == "B") return 2 + 3;
-                if (p1 == "C") return 3 + 3;
-                break;
-            
-            // win
-            case "Z":
-                if (p1 == "A") return 2 + 6;
-                if (p1 == "B") return 3 + 6;
-                if (p1 == "C") return 1 + 6;
-                break;
-        }
+        // 0 1 - 1
+        // 1 2 - 3
+        // 2 0 - 2
         
-        return 0;
+        // lose
+        // 0 2 - 2
+        // 1 0 - 1
+        // 2 1 - 3
+        
+        // draw
+
+        return (p2 * 3) + (p1 + p2 + 2) % 3 + 1;
     }
 }
